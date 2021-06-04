8d497fa608b1de17174316fe2914ebeebbd9d926
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2021-06-04 17:03:44 +0530

    login page design and routing adjustment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule)},
   { path: 'home', loadChildren: () => import('./before-login/before-login.module').then(m => m.BeforeLoginModule) },
   { path: 'after-login', loadChildren: () => import('./after-login/after-login.module').then(m => m.AfterLoginModule) }
 ];
