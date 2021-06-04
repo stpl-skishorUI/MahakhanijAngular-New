cab93585d253be1afe063e456a12fa29b99b549a
stpl-Pramod <pramod.bhamare@shauryatechnosoft.com>
2021-06-04 17:14:12 +0530

    add About components

---
+++ src/app/before-login/about/district-mineral-foundation/district-mineral-foundation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-district-mineral-foundation',
+  templateUrl: './district-mineral-foundation.component.html',
+  styleUrls: ['./district-mineral-foundation.component.css']
+})
+export class DistrictMineralFoundationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/before-login/before-login-routing.module.ts
+++ src/app/before-login/before-login-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-
+import { SystemOverviewComponent } from './about/system-overview/system-overview.component';
+import { AboutMineralsComponent } from './about/about-minerals/about-minerals.component';
+import { DistrictMineralFoundationComponent } from './about/district-mineral-foundation/district-mineral-foundation.component';
  
 const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent },
+    { path: 'system-overview', component: SystemOverviewComponent },
+    { path: 'about-minerals', component: AboutMineralsComponent },
+    { path: 'district-mineral-foundation', component: DistrictMineralFoundationComponent }
 ];
 
 @NgModule({
