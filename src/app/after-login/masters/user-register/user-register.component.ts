41683bc139ff359a5b27cd9dca8e99b53d2b6dbe
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2021-06-21 10:16:01 +0530

    user registration page added

--- src/app/after-login/masters/user-register/user-register.component.ts
+++ src/app/after-login/masters/user-register/user-register.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-user-register',
   templateUrl: './user-register.component.html',
---
+++ src/app/after-login/masters/user-register/user-register.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    imports: [
+    ],
+    declarations: [
+        
+    ],
+    providers: [],
+    exports: [
+    ]
+})
+
+export class UserRegisterModule { }
