15e7ad521fd6b5d51bae26d7824f25bc8e383bf4
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2021-06-23 19:03:44 +0530

    added ngx-select

--- src/app/after-login/masters/user-register/user-register.component.ts
+++ src/app/after-login/masters/user-register/user-register.component.ts
@@ -5,6 +5,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-register.component.css', '/../../after-login.component.css']
 })
 export class UserRegisterComponent implements OnInit {
+  public items: string[] = ['Amsterdam', 'Antwerp', 'Athens', 'Barcelona',
+        'Berlin', 'Birmingham', 'Bradford', 'Bremen', 'Brussels', 'Bucharest',
+        'Budapest', 'Cologne', 'Copenhagen', 'Dortmund', 'Dresden', 'Dublin',
+        'Düsseldorf', 'Essen', 'Frankfurt', 'Genoa', 'Glasgow', 'Gothenburg',
+        'Hamburg', 'Hannover', 'Helsinki', 'Kraków', 'Leeds', 'Leipzig', 'Lisbon',
+        'London', 'Madrid', 'Manchester', 'Marseille', 'Milan', 'Munich', 'Málaga',
+        'Naples', 'Palermo', 'Paris', 'Poznań', 'Prague', 'Riga', 'Rome',
+        'Rotterdam', 'Seville', 'Sheffield', 'Sofia', 'Stockholm', 'Stuttgart',
+        'The Hague', 'Turin', 'Valencia', 'Vienna', 'Vilnius', 'Warsaw', 'Wrocław',
+        'Zagreb', 'Zaragoza', 'Łódź'];
   expandedRowIndex :number=-1;
   tableData = [
     {
