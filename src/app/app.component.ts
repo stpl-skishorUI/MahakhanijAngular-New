6f4d8494f4f6892b822a2660c209ac8df5cd75d7
pramod.bhamare <pramod.bhamare@shauryatechnosoft.com>
2021-06-25 19:04:34 +0530

    before pages changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Router, NavigationStart, NavigationEnd, NavigationError } from '@angula
 })
 export class AppComponent {
   title = 'MahakhanijAngular-New';
-  isBeforeLoginPage: boolean = false;
+  isBeforeLoginPage: boolean = true;
   isLoginPage: boolean = true;
   constructor(
 
